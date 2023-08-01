64c7df4c5d47c878aab13e59014a80a408c7311a
CokeLee777 <akak640@kyonggi.ac.kr>
2023-08-01 15:15:34 +0900

    20230801-3

--- day01/src/day01/Main.java
+++ day01/src/day01/Main.java
@@ -3,6 +3,7 @@ package day01;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello1111111");
 		System.out.println("Hello2222222");
 		System.out.println("Hello3333333");
 	}
