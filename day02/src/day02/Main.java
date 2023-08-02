a2c4454220ca8e579d36a62498ed6cdc714cb410
CokeLee777 <akak640@kyonggi.ac.kr>
2023-08-02 09:18:27 +0900

    init: project initialize

---
+++ day02/src/day02/Main.java
@@ -0,0 +1,15 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String cmd = sc.next();
+		}
+	}
+
+}
